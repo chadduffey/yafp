f35f6f62cbf41df72729b607a4f1646dfa155fb8
Chad Duffey <7192541+chadduffey@users.noreply.github.com>
2019-05-12 11:00:33 -0700

    basic templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello Again!"
+	user = {'username':'chad'}
+	posts = [
+        {
+            'author': {'username': 'duff'},
+            'body': 'Beautiful day in Seattle!'
+        },
+        {
+            'author': {'username': 'jwess'},
+            'body': 'The Nun! What a movie!'
+        }
+    ]
+	return render_template('index.html', title='Home', user=user, posts=posts)
+
