f5224f67b5ca10dad7d751227fbe70d31be6ff69
Chad Duffey <7192541+chadduffey@users.noreply.github.com>
2019-05-26 10:59:25 -0700

    following relationships

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 @login.user_loader
 def load_user(id):
 	return User.query.get(int(id))
@@ -19,6 +24,12 @@ class User(UserMixin, db.Model):
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 	
+	followed = db.relationship(
+				'User', secondary=followers,
+				primaryjoin=(followers.c.follower_id == id),
+				secondaryjoin=(followers.c.follower_id == id),
+				backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 
@@ -33,6 +44,18 @@ class User(UserMixin, db.Model):
 		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
 			digest, size)
 
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+
+	def is_following(self, user):
+		return self.followed.filter(
+			followers.c.followed_id == user_id).count() > 0
+
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -42,3 +65,6 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return '<Post {}>'.format(self.body)
+
+
+
