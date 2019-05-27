be3f1380695ef526a29ca0ec1c082c1d2f9d608c
Chad Duffey <7192541+chadduffey@users.noreply.github.com>
2019-05-27 06:46:43 -0700

    add following relationships and tests

--- app/models.py
+++ app/models.py
@@ -1,18 +1,15 @@
 from datetime import datetime
 from hashlib import md5
+from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db, login
 
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+followers = db.Table(
+	'followers',
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
-
-@login.user_loader
-def load_user(id):
-	return User.query.get(int(id))
 
 
 class User(UserMixin, db.Model):
@@ -23,12 +20,11 @@ class User(UserMixin, db.Model):
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-	
 	followed = db.relationship(
-				'User', secondary=followers,
-				primaryjoin=(followers.c.follower_id == id),
-				secondaryjoin=(followers.c.follower_id == id),
-				backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+		'User', secondary=followers,
+		primaryjoin=(followers.c.follower_id == id),
+		secondaryjoin=(followers.c.followed_id == id),
+		backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
@@ -54,7 +50,19 @@ class User(UserMixin, db.Model):
 
 	def is_following(self, user):
 		return self.followed.filter(
-			followers.c.followed_id == user_id).count() > 0
+			followers.c.followed_id == user.id).count() > 0
+
+	def followed_posts(self):
+		followed = Post.query.join(
+			followers, (followers.c.followed_id == Post.user_id)).filter(
+				followers.c.follower_id == self.id)
+		own = Post.query.filter_by(user_id=self.id)
+		return followed.union(own).order_by(Post.timestamp.desc())
+
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -65,6 +73,3 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return '<Post {}>'.format(self.body)
-
-
-
--- app/routes.py
+++ app/routes.py
@@ -10,9 +10,9 @@ from app.models import User
 
 @app.before_request
 def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow()
+		db.session.commit()
 
 
 @app.route('/')
@@ -20,15 +20,15 @@ def before_request():
 @login_required
 def index():
 	posts = [
-        {
-            'author': {'username': 'duff'},
-            'body': 'Beautiful day in Seattle!'
-        },
-        {
-            'author': {'username': 'jwess'},
-            'body': 'The Nun! What a movie!'
-        }
-    ]
+		{
+			'author': {'username': 'duff'},
+			'body': 'Beautiful day in Seattle!'
+		},
+		{
+			'author': {'username': 'jwess'},
+			'body': 'The Nun! What a movie!'
+		}
+	]
 	return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -55,17 +55,17 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Congratulations, you are now a registered user!')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
 
 @app.route('/user/<username>')
 @login_required
@@ -91,3 +91,33 @@ def edit_profile():
 		form.username.data = current_user.username
 		form.about_me.data = current_user.about_me
 	return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('User {} not found.'.format(username))
+		return redirect(url_for('index'))
+	if user == current_user:
+		flash('You cannot follow yourself!')
+		return redirect(url_for('user', username=username))
+	current_user.follow(user)
+	db.session.commit()
+	flash('You are following {}!'.format(username))
+	return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('User {} not found.'.format(username))
+		return redirect(url_for('index'))
+	if user == current_user:
+		flash('You cannot unfollow yourself!')
+		return redirect(url_for('user', username=username))
+	current_user.unfollow(user)
+	db.session.commit()
+	flash('You are not following {}.'.format(username))
+	return redirect(url_for('user', username=username))
