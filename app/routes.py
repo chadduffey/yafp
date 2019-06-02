f37607e812b447cd2ef34925c31660ee0e00b0d4
Chad Duffey <7192541+chadduffey@users.noreply.github.com>
2019-06-02 10:21:49 -0700

    password reset routes and email functionality

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from app import app, db
 from app.email import send_password_reset_email
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequest
+from app.forms import ResetPasswordForm 
 from app.models import User, Post
 
 @app.before_request
@@ -150,7 +151,7 @@ def explore():
 def reset_password_request():
 	if current_user.is_authenticated:
 		return redirect(url_for('index'))
-	form = ResetPasswordRequestForm()
+	form = ResetPasswordRequest()
 	if form.validate_on_submit():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user:
@@ -159,3 +160,18 @@ def reset_password_request():
 		return redirect(url_for('login'))
 	return render_template('reset_password_request.html',
 						   title='Reset Password', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	user = User.verify_reset_password_token(token)
+	if not user:
+		return redirect(url_for('index'))
+	form = ResetPasswordForm()
+	if form.validate_on_submit():
+		user.set_password(form.password.data)
+		db.session.commit()
+		flash('Your password has been reset.')
+		return redirect(url_for('login'))
+	return render_template('reset_password.html', form=form)
